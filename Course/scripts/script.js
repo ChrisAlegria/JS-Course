f55e55deb41137faa165d85055c6fa2af897523f
Chris Alegria <christian.alegriar@gmail.com>
2025-07-03 21:42:06 -0600

    Add: Initial Structure To JS Course

---
+++ Course/scripts/script.js
@@ -0,0 +1,6 @@
+/* JavaScript: Es el sistema nervioso y el que ejecuta las acciones. */
+/* Comentario y comentario multilinea: Texto o codigo que solamente el desarrollador puede ver, ademas estos no tendran ningun efecto en lo que visualiza el usuario, por lo que de manera global y en cualquier lenguaje se puede generar un comentarios con Ctrl + }. */
+
+
+// ^JavaScript
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
