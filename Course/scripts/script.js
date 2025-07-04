2034b2a3ac4ab9cd9e8a106f18568128cc3fdbea
Chris Alegria <christian.alegriar@gmail.com>
2025-07-03 22:03:47 -0600

    Add: Kind Of Variables Section

--- Course/scripts/script.js
+++ Course/scripts/script.js
@@ -1,6 +1,26 @@
 /* JavaScript: Es el sistema nervioso y el que ejecuta las acciones. */
 /* Comentario y comentario multilinea: Texto o codigo que solamente el desarrollador puede ver, ademas estos no tendran ningun efecto en lo que visualiza el usuario, por lo que de manera global y en cualquier lenguaje se puede generar un comentarios con Ctrl + }. */
-
+/* CamelCase: El camelCase es una convención de escritura que se utiliza en programación para nombrar variables, funciones, clases, etc. Se escribe en minúsculas y cada palabra adicional comienza con mayúscula. Por ejemplo, "miVariable", "miFuncion", "miClasePersonalizada". */
 
 // ^JavaScript
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ~Variables en JS
+// ~Las variables dentro de JS son contenedores donde se puede almacenar valores, como texto, numeros, objetos, arreglos, etc.. Ademas que existen 3 maneras de diferentes para declararlas.
+// Todo - Declaración de Variables en JavaScript: En JavaScript existen tres formas principales de declarar una variable: `var`, `let` y `const`. Aunque cada una tiene diferencias clave en su comportamiento (como el alcance o si permite reasignación), todas siguen una estructura básica común:  **palabraClave + nombreVariable + = + valor**. Primero se escribe la palabra clave (`let`, `const` o `var`), seguida del nombre de la variable, después el signo igual `=` que funciona como asignador, y finalmente el valor que se desea almacenar. Por ejemplo: `let nombre = "Juan";`. Esta sintaxis permite crear variables claras y organizadas que se pueden usar a lo largo del código según las necesidades del programa. */
+var //* Palabra Clave
+nombreDeLaVariable //* Nombre de la variable
+= //*Asignador
+'Valor'//* Valor de la variable
+
+// &Var
+// &Las variables tipo 'Var', 
+var variableTipoVar = 'Esta es una variable de tipo var.'
+
+//&Let
+// &
+var variableTipoLet = 'Esta es una variable de tipo let.'
+
+
+// &//&Const
+var variableTipoConst = 'Esta es una variable de tipo const.'
+
