73ad55b646abfada3c21865a4ee14ed96d6cbf3c
Chris Alegria <christian.alegriar@gmail.com>
2025-07-23 14:01:49 -0600

    Add: Project 2 Message Colors

--- Projects/Project_2/scripts/script.js
+++ Projects/Project_2/scripts/script.js
@@ -7,11 +7,14 @@ function adivinarNumeroAleatorio(){
     const valorUsuaurio = document.getElementById('numeroEntrada').value;
     if(valorUsuaurio >= 1 && valorUsuaurio <=100 ){
         if(valorUsuaurio == Math.trunc(numeroAleatorio)){
-        mensaje.textContent =`Has acertado, el numero es ${numeroAleatorio}`;
+            mensaje.textContent =`Has acertado, el numero es ${numeroAleatorio}`;
+            mensaje.style = "color: greenyellow";
         } else if(valorUsuaurio > numeroAleatorio){
             mensaje.textContent = `Te has pasado, es un menor`;
+            mensaje.style = "color: red";
         } else {
             mensaje.textContent =`Aun estas por debajo del numero`;
+            mensaje.style = "color: blue";
         }
     } else{
         alert('Debes de escribir un valor valido entre el 1 y el 100');
