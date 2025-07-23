832325e1d2b1c8d709008f4e35ebf1ceec0d418c
Chris Alegria <christian.alegriar@gmail.com>
2025-07-23 14:11:49 -0600

    Add: Disable Input

--- Projects/Project_2/scripts/script.js
+++ Projects/Project_2/scripts/script.js
@@ -7,17 +7,18 @@ function adivinarNumeroAleatorio(){
     const valorUsuaurio = document.getElementById('numeroEntrada').value;
     if(valorUsuaurio >= 1 && valorUsuaurio <=100 ){
         if(valorUsuaurio == Math.trunc(numeroAleatorio)){
-            mensaje.textContent =`Has acertado, el numero es ${numeroAleatorio}`;
+            mensaje.textContent =`¡Felicitaciones!, ¡has adivinado el numero, el cual es: Fix: ${numeroAleatorio}.!`;
             mensaje.style = "color: greenyellow";
+            numeroEntrada.disabled = true;
         } else if(valorUsuaurio > numeroAleatorio){
-            mensaje.textContent = `Te has pasado, es un menor`;
+            mensaje.textContent = `¡Te has pasado!, es un numero menor.`;
             mensaje.style = "color: red";
         } else {
-            mensaje.textContent =`Aun estas por debajo del numero`;
+            mensaje.textContent =`¡Mas alto!, es un numero mayor.`;
             mensaje.style = "color: blue";
         }
     } else{
-        alert('Debes de escribir un valor valido entre el 1 y el 100');
+        alert('Introduce un numero valido entre el 1 y el 100.');
         document.getElementById('numeroEntrada').value = ''
     }
 }
