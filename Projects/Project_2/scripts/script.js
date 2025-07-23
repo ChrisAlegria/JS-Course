05ec564c787b26682b0d4fb9d4b57d113823a30e
Chris Alegria <christian.alegriar@gmail.com>
2025-07-23 13:33:11 -0600

    Add: Commets At Js Project 2

--- Projects/Project_2/scripts/script.js
+++ Projects/Project_2/scripts/script.js
@@ -1,9 +1,10 @@
-const numeroAleatorio = Math.trunc(Math.random()*100);
+// *Se declara la variable que almacenara el numero al azar.
+const numeroAleatorio = Math.floor(Math.random()*100)+1;
 console.log(numeroAleatorio)
 
+// *Esta funcion se ejecutara cuando el usuario haga click en el btn.
 function adivinarNumeroAleatorio(){
     const valorUsuaurio = document.getElementById('numeroEntrada').value;
-
     if(valorUsuaurio == Math.trunc(numeroAleatorio)){
         alert(`Has acertado, el numero es ${numeroAleatorio}`);
     } else if(valorUsuaurio > numeroAleatorio){
