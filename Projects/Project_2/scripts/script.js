9bb99a41fe98dc8814736a2b0979d509b10dbc34
Chris Alegria <christian.alegriar@gmail.com>
2025-07-23 13:56:30 -0600

    Fix: Code Structure

--- Projects/Project_2/scripts/script.js
+++ Projects/Project_2/scripts/script.js
@@ -5,11 +5,16 @@ console.log(numeroAleatorio)
 // *Esta funcion se ejecutara cuando el usuario haga click en el btn.
 function adivinarNumeroAleatorio(){
     const valorUsuaurio = document.getElementById('numeroEntrada').value;
-    if(valorUsuaurio == Math.trunc(numeroAleatorio)){
-        alert(`Has acertado, el numero es ${numeroAleatorio}`);
-    } else if(valorUsuaurio > numeroAleatorio){
-        alert(`Te has pasado, es un menor`);
-    } else {
-        alert(`Aun estas por debajo del numero`)
+    if(valorUsuaurio >= 1 && valorUsuaurio <=100 ){
+        if(valorUsuaurio == Math.trunc(numeroAleatorio)){
+        mensaje.textContent =`Has acertado, el numero es ${numeroAleatorio}`;
+        } else if(valorUsuaurio > numeroAleatorio){
+            mensaje.textContent = `Te has pasado, es un menor`;
+        } else {
+            mensaje.textContent =`Aun estas por debajo del numero`;
+        }
+    } else{
+        alert('Debes de escribir un valor valido entre el 1 y el 100');
+        document.getElementById('numeroEntrada').value = ''
     }
 }
