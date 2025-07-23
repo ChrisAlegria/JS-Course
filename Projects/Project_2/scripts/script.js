f714540fc935528e8b3b1c6808a723e6f220e017
Chris Alegria <christian.alegriar@gmail.com>
2025-07-23 16:02:02 -0600

    Update: General Structure At Project 2

--- Projects/Project_2/scripts/script.js
+++ Projects/Project_2/scripts/script.js
@@ -1,22 +1,32 @@
 // *Se declara la variable que almacenara el numero al azar.
-const numeroAleatorio = Math.floor(Math.random()*100)+1;
-console.log(numeroAleatorio)
+let numeroAleatorio = Math.floor(Math.random()*100)+1;
+console.log(numeroAleatorio);
+
+let contadorIntentos = 0;
+
+intentos.textContent = `Llevas un total de ${contadorIntentos} intentos.`;
 
 // *Esta funcion se ejecutara cuando el usuario haga click en el btn.
 function adivinarNumeroAleatorio(){
     const valorUsuaurio = document.getElementById('numeroEntrada').value;
     if(valorUsuaurio >= 1 && valorUsuaurio <=100 ){
+        contadorIntentos++; 
+        intentos.textContent = `Llevas un total de ${contadorIntentos} intentos.`;
+
         if(valorUsuaurio == Math.trunc(numeroAleatorio)){
-            mensaje.textContent =`¡Felicitaciones!, ¡has adivinado el numero, el cual es: Fix: ${numeroAleatorio}.!`;
+            mensaje.textContent =`¡Felicitaciones!, ¡has adivinado el numero, el cual es: ${numeroAleatorio}.!`;
             mensaje.style = "color: greenyellow";
             numeroEntrada.disabled = true;
+
         } else if(valorUsuaurio > numeroAleatorio){
             mensaje.textContent = `¡Te has pasado!, es un numero menor.`;
             mensaje.style = "color: red";
+
         } else {
             mensaje.textContent =`¡Mas alto!, es un numero mayor.`;
             mensaje.style = "color: blue";
         }
+
     } else{
         alert('Introduce un numero valido entre el 1 y el 100.');
         document.getElementById('numeroEntrada').value = ''
