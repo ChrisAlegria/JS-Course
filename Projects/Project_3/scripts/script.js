3abb2f77c74705d908ab097d52dec49c8ddf9b9f
Chris Alegria <christian.alegriar@gmail.com>
2025-08-18 14:20:26 -0600

    Update: Variables Declaratiosn At Project 3

--- Projects/Project_3/scripts/script.js
+++ Projects/Project_3/scripts/script.js
@@ -1,3 +1,7 @@
-function test(){
-    console.log('si funciona')
-}
+import { barcelona, roma, paris, londres } from "./ciudades";
+
+let enlaces = document.querySelectorAll('a');
+let tituloElemento = document.getElementById('titulo');
+let subtituloElemento = document.getElementById('subtitulo');
+let parrafoElemento = document.getElementById('parrafo');
+
