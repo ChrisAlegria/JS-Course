52cf73f59c2aa900dbae54b8e126b3e727769fd3
Chris Alegria <christian.alegriar@gmail.com>
2025-09-15 21:07:34 -0600

    Test: Firebase Fetch

--- Projects/Project_5/scripts/script.js
+++ Projects/Project_5/scripts/script.js
@@ -1,3 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDCRUaLNVg-tE8rgFomfxWF2JVdByRcSS4",
+    authDomain: "proyecto---formulario-js-52c33.firebaseapp.com",
+    projectId: "proyecto---formulario-js-52c33",
+    storageBucket: "proyecto---formulario-js-52c33.firebasestorage.app",
+    messagingSenderId: "38906877185",
+    appId: "1:38906877185:web:972b07f10d45b270c67700",
+    measurementId: "G-WV1XMTGRVN"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = firebase.firestore();
+
 document.getElementById('formulario').addEventListener('submit', (event) => {
     event.preventDefault();
 
@@ -41,11 +60,21 @@ document.getElementById('formulario').addEventListener('submit', (event) => {
     //*Envio de formulario si todos los campos son validos
     if(campoNombre && correoPatern.test(campoCorreo) && contrasenaPatern.test(campoContrasena)){
         alert('Se envio el formulario correctamente.');
+        envioInformacionFirebase(campoNombre, campoCorreo, campoContrasena)
     }
 
     //*Funcion de envio de los datos a Firebase
-    async function envioDatosFirebase(){
-        fetch('')
-        .then
+    async function envioInformacionFirebase(nombre, correo, contrasena,){
+        db.collection("users").add({
+            nombre: nombre,
+            email: correo,
+            password: contrasena
+        })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
     }
-});
+})
